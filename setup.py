3b522d9416102e30902b19911500ce8c149efde7
Alessandro Thea <alessandro.thea@cern.ch>
2023-11-22 14:02:50 +0100

    Adding submodule initialisation

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@
 from pybind11.setup_helpers import Pybind11Extension, build_ext, ParallelCompile
 from setuptools import setup, find_packages
 from setuptools.command.install import install
+from setuptools.command.build import _build
+import subprocess
+
 import os.path as path
 
 from glob import glob
@@ -13,7 +16,8 @@ dunedaq_packages = [
     'detdataformats',
     'fddetdataformats',
     'daqdataformats',
-    # 'detchannelmaps'
+    # 'detchannelmaps',
+    'rawdatautils'
 ]
 
 packages = dunedaq_packages[:]
@@ -61,6 +65,23 @@ package_dir = { p:f"dunedaq/{p}/python/{p}" for p in dunedaq_packages }
 #     ),
 # ]
 
+
+class build(_build):
+    def run(self):
+        dir_path = path.dirname(path.realpath(__file__))
+        sm_init_res = subprocess.check_output(['git', 'submodule', 'update', '--init', '--recursive'], cwd=dir_path)
+        print(sm_init_res)
+
+
+        # ---- some custom code here ----
+        # important to do this instead of "super", since it hasn't been properly updated
+        _build.run(self) 
+
+setup(
+    # setup specifications here
+    cmdclass={"build": build}
+)
+
 ext_modules = [
     Pybind11Extension(
         f"{p}/_daq_{p}_py",
@@ -92,6 +113,7 @@ with ParallelCompile("NPY_NUM_BUILD_JOBS"):
         # Currently, build_ext only provides an optional "highest supported C++
         # level" feature, but in the future it may provide more features.
         cmdclass={
+            "build": build
             "build_ext": build_ext,
             },
         zip_safe=False,
